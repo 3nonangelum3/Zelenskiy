bcd79400e45f04d1d66c2e2a61245b9b5c8b51d0
3nonangelum3 <93791126+3nonangelum3@users.noreply.github.com>
2022-09-28 04:47:18 +0200

    Update LocalizationData.py

--- Localization/LocalizationData.py
+++ Localization/LocalizationData.py
@@ -1,16 +1,25 @@
-varList = {"gameLoad": ["Do you want to load your previous game?\n",
-                        "Хотите загрузить свою предыдущую игру?\n",
-                        "Бажаєте завантажити свою попередню гру?\n"],
-           "": []}
+varList = {"gameLoad": ["Do you want to load your previous game?",
+                        "Хотите загрузить свою предыдущую игру?",
+                        "Бажаєте завантажити свою попередню гру?"],
+           "getCode": ["Please input code given during last saving\n"
+                       "If there is NO code, please enter <Nothing>",
+                       "Пожалуйста, введите код, полученный при последнем сохранении\n"
+                       "Если кода НЕТ, введите <Ничего>",
+                       "Будь ласка, введіть код, наданий під час останнього збереження\n"
+                       "Якщо коду НЕМАЄ, введіть <Нічого>"],
+           }
 valueList = [{
-                "Yes": True,
-                "No": False,
-            },
-            {
-                "Да": True,
-                "Нет": False,
-            },
-            {
-                "Так": True,
-                "Ні": False,
-            }]
+    "Yes": True,
+    "No": False,
+    "Nothing": "Stop",
+},
+{
+    "Да": True,
+    "Нет": False,
+    "Ничего": "Stop",
+},
+{
+    "Так": True,
+    "Ні": False,
+    "Нічого": "Stop",
+}]

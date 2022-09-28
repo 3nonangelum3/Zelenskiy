ff24a1f2a94ab92accbb259c739919d23e6bc51b
3nonangelum3 <93791126+3nonangelum3@users.noreply.github.com>
2022-09-28 03:09:08 +0200

    Update LocalizationAPI.py

--- Localization/LocalizationAPI.py
+++ Localization/LocalizationAPI.py
@@ -1,16 +1,28 @@
+import Localization.LocalizationData
+
+
 def getLanguage() -> str:
     """
     Asks user to input a language of an interface until valid language will be inputted
     :return: Language inputted by user
     """
-    languageInnerVar = ""
-    while languageInnerVar == "" or ["English", "Русский язык", "Українська мова"].index(languageInnerVar) is None:
-        languageInnerVar = input("What language do you prefer?\n"
+    language = ""
+    while language == "":
+        language = input("What language do you prefer?\n"
                          "Какой язык Вы предпочитаете?\n"
                          "Якій мові Ви надаєте перевагу?\n"
                          "English, Русский язык, Українська мова\n"
                          ">>> ")
-    return languageInnerVar
+        try:
+            ["English", "Русский язык", "Українська мова"].index(language)
+        except ValueError:
+            language = ""
+    return language
 
 
-def Localise(Language: int, varName: str):
+def getIndex(language: str):
+    pass
+
+
+def Localise(language: str, varName: str):
+    if language in Localization

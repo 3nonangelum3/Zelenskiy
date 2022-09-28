557f9d3400967371df409241470cbdc22116d070
3nonangelum3 <93791126+3nonangelum3@users.noreply.github.com>
2022-09-28 04:47:45 +0200

    saving progress

--- Game/GameApi.py
+++ Game/GameApi.py
@@ -19,7 +19,21 @@ def loadGame() -> bool:
     load = ""
     while load == "":
         load = input(f"{localise(language, 'gameLoad')}\n\n>>> ")
+        load = unlocalize(language, load)
+        if load:
+            try:
+                if load == "False":
+                    return False
+                elif load == "True":
+                    return True
+                else:
+                    return None
+            except TypeError:
+                load = ""
 
+
+def getDataCode(code: str):
+    return {}
 
 
 def gameInit():
@@ -29,15 +43,33 @@ def gameInit():
     """
     global language
     language = getLanguage()
-
+    dataLoaded = False
+    dataList = {}
+    loading = loadGame()
+    if loading:
+        code = input(f"{localise(language, 'getCode')}\n>>> ")
+        if unlocalize(language, code) == "Stop":
+            print("Loading saved game stopped")
+        else:
+            dataList = getDataCode(code)
+            dataLoaded = True
     global params, currentOfferIndex, maxOfferInder, daysAlive
-    params["influence"] = 50
-    params["people"] = 50
-    params["money"] = 50
-    params["army"] = 50
-    maxOfferIndex = 0
-    currentOfferIndex = 10
-    daysAlive = 0
+    if dataLoaded:
+        params["influence"] = dataList["influence"]
+        params["people"] = dataList["people"]
+        params["money"] = dataList["money"]
+        params["army"] = dataList["army"]
+        maxOfferIndex = 0
+        currentOfferIndex = dataList["currentOfferIndex"]
+        daysAlive = dataList["daysAlive"]
+    else:
+        params["influence"] = 50
+        params["people"] = 50
+        params["money"] = 50
+        params["army"] = 50
+        maxOfferIndex = 0
+        currentOfferIndex = 0
+        daysAlive = 0
 
 
 def gameStart():

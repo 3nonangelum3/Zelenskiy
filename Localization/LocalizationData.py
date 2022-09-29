1bc75c3aba2f2766b6e4081d94dd0cff13a141b3
3nonangelum3 <93791126+3nonangelum3@users.noreply.github.com>
2022-09-29 03:24:50 +0200

    Update LocalizationData.py

--- Localization/LocalizationData.py
+++ Localization/LocalizationData.py
@@ -1,7 +1,15 @@
-varList = {"gameLoad": ["Do you want to load your previous game?\n",
-                        "Хотите загрузить свою предыдущую игру?\n",
-                        "Бажаєте завантажити свою попередню гру?\n"],
-           "": []}
+varList = {
+    "gameLoad": [
+        "Do you want to load your previous game?\n",
+        "Хотите загрузить свою предыдущую игру?\n",
+        "Бажаєте завантажити свою попередню гру?\n"
+    ],
+    "foreword": [
+        "",
+        "",
+        ""
+    ]
+}
 valueList = [{
                 "Yes": True,
                 "No": False,
